0cba55c30e12f90d25676034d8728dea435ce2bb
Khemraj Thapa <khyamay@gmail.com>
2014-07-01 00:57:58 +0100

    added partials

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,15 +7,32 @@ angular.module('F1FeederApp.controllers', [])
   	$scope.nameFilter = null;
   	$scope.driversList = [];
 
+  	$scope.searchFilter = function (driver){
+  		var keyword = new RegExp($scope.nameFilter, 'i');
+  		return !$scope.nameFilter || keyword.test(driver.Driver.givenName) || keyword.test(driver.Driver.familyName);
+  	};
+
+
   	ergastAPIService.getDrivers().success(function (response){
   	$scope.driversList = response.MRData.StandingsTable.StandingsLists[0].DriverStandings;
 
   	});
 
-  	$scope.searchFilter = function (driver){
-  		var keyword = new RegExp($scope.nameFilter, 'i');
-  		return !$scope.nameFilter || keyword.test(driver.Driver.givenName) || keyword.test(driver.Driver.familyName);
-  	};
+  }]).
+  controller('driverCtrl', ['$scope','$routeParams', 'ergastAPIService', function($scope, $routeParams, ergastAPIService){
+  	$scope.id = $routeParams.id;
+  	$scope.races = [];
+  	$scope.driver = null;
+
+  	ergastAPIService.getDriverDetails($scope.id)
+  		.success(function (resposne){
+  			$scope.driver = response.MRData.StandingsTable.StandingsLists[0].DriverStandings[0];
+  		});
+
+  	ergastAPIService.getDriverRaces($scope.id)
+  		.success(function (response){
+  			$scope.races = response.MRData.RaceTable.Races;
+  		});
   }]);
 
  
--- app/js/services.js
+++ app/js/services.js
@@ -14,6 +14,20 @@ angular.module('F1FeederApp.services', []).
 				method: 'JSONP',
         		url: 'http://ergast.com/api/f1/2013/driverStandings.json?callback=JSON_CALLBACK'
 			});
+		};
+
+		ergastAPI.getDriversDetails = function(id){
+			return $http({
+				method: 'JSONP',
+				url: 'http://ergast.com/api/f1/2013/drivers/' + id +'/driverStandings.json?callback=JSON_CALLBACK'
+			})
+		};
+
+		ergastAPI.getDriverRaces = function (id){
+			return $http({
+				method: 'JSONP',
+				url: 'http://ergast.com/api/f1/2013/drivers/' + id + '/results.json?callback=JSON_CALLBACK'
+			})
 		}
 
 		return ergastAPI;
